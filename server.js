1987e3f213533038c0eb75b7b31c870376fc97b1
lachlanmaclean <admin@cubecrafted.com>
2019-06-04 22:35:39 +1200

    It plays youtube links. Sounds like garbage. but plays youtube links.

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const _config = require('./config');
+const ytdl = require('ytdl-core');
+const {TOKEN, PREFIX} = require('./config');
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     //Commands when bot first logs in
     console.log("Servers Connected to: ");
     client.guilds.forEach((guild) => {
-        console.log(" - " + guild.name)
+        console.log(" - " + guild.name);
 
         guild.channels.forEach((channel) => {
             console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
@@ -14,10 +15,44 @@ client.on('ready', () => {
     })
 });
 
-client.on('message', msg => {
-    if (msg.content === '!help') {
-        msg.reply(`\nList of commands: \n!memes - Grabs a random zesty meme from the internet \n!song (Youtube link) - to play a song from Youtube in a voice channel`);
+client.on('message', async msg => {
+    if (msg.author.bot) return undefined;
+    if (!msg.content.startsWith(PREFIX)) return undefined;
+    const args = msg.content.split(' ');
+    let connection;
+    if (msg.content.startsWith(`${PREFIX}play`)) {
+        const voiceChannel = msg.member.voiceChannel;
+        if (!voiceChannel) return msg.channel.send('You need to be in a voice channel');
+        const permissions = voiceChannel.permissionsFor(msg.client.user);
+        if (!permissions.has('CONNECT')) {
+            return msg.channel.send('I don\'t have permission to connect.');
+        }
+        if (!permissions.has('SPEAK')) {
+            return msg.channel.send('I don\'t have permission to speak in the voice channel.');
+        }
+
+        try {
+            connection = await voiceChannel.join();
+        } catch (error) {
+            console.error(error);
+            return msg.channel.send('Error: Couldn\'t join channel.');
+
+        }
+        const dispatcher = connection.playOpusStream(ytdl(args[1]))
+            .on('end', () => {
+                console.log('Song Ended!');
+                voiceChannel.leave();
+            }).on('error', error => {
+                console.log(error);
+            });
+
+        dispatcher.setVolumeLogarithmic(5 / 5);
     }
 });
 
-client.login(_config.server.token);
+client.login(TOKEN);
+
+
+//if (msg.content === '!help') {
+    //msg.reply(`\nList of commands: \n!memes - Grabs a random zesty meme from the internet \n!song (Youtube link) - to play a song from Youtube in a voice channel`);
+//}
